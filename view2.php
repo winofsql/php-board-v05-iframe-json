c44637be82ef49771572fa4163c6c7388a0236f4
sworc <lightbox@nifty.com>
2022-03-10 11:27:49 +0900

    Add files via upload

--- view2.php
+++ view2.php
@@ -6,7 +6,7 @@
     <title>掲示板</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="client.css?_=<?= time() ?>">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 </head>
 
@@ -14,9 +14,9 @@
 <div id="data_head" class="alert alert-primary">投稿一覧 (<?= $kensu ?>件)</div>
 <div id="data_body">
 
-	<div id="data_entry">
-		<?= $log_text ?>
-	</div>
+    <div id="data_entry">
+        <?= $log_text ?>
+    </div>
 </div>
 </body>
 </html>
